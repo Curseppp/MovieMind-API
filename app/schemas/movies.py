49c5bf1ac7c6161e27d6fbdc7a071880c76b3517
Curseppp <imenzorus@gmail.com>
2026-07-09 14:06:55 +0300

    feat(movies): expose poster URL and genre names

--- app/schemas/movies.py
+++ app/schemas/movies.py
@@ -6,9 +6,19 @@ class Genre(BaseModel):
     name: str
 
 
-class PublicMovie(BaseModel):
+class Movie(BaseModel):
     original_title: str
     release_date: str
     genres: list[Genre]
     vote_average: float
     vote_count: int
+    poster_url: str | None
+
+
+class PublicMovie(BaseModel):
+    original_title: str
+    release_date: str
+    genres: list[str]
+    vote_average: float
+    vote_count: int
+    poster_url: str | None
--- app/services/movies.py
+++ app/services/movies.py
@@ -4,13 +4,15 @@ from app.services.tmdb import tmdb_client, TmdbLanguage
 
 def get_movie_details(movie_id: str, language: TmdbLanguage) -> PublicMovie:
     movie = tmdb_client.get_movie_by_id(movie_id, language)
+    poster_path = movie.get("poster_path")
 
     details = PublicMovie(
         original_title=movie["original_title"],
         release_date=movie["release_date"],
-        genres=movie["genres"],
+        genres=[genre["name"] for genre in movie["genres"]],
         vote_average=movie["vote_average"],
         vote_count=movie["vote_count"],
+        poster_url=tmdb_client.get_tmdb_poster_url(poster_path),
     )
 
     return details
